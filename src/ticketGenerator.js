4cfd2aa6f171de4f45291525a2a4e993a533d726
wendyhamel <whamel@it4web.nl>
2026-04-28 17:50:04 +0200

    Working submit of form with preview of avatar

--- src/ticketGenerator.js
+++ src/ticketGenerator.js
@@ -4,37 +4,45 @@ window.getTicket = function() {
 		fullName: '',
 		email: '',
 		GHUsername: '',
-		requestEarlyAccess: {},
-		submitted: true,
+		avatarPreview: null,
+		requestEarlyAccess: {
+			avatarPreview: null,
+			fullName: '',
+			email: '',
+			GHUsername: '',
+		},
+		submitted: false,
 		validation: {
 			avatar: {
 				rule: {
 					required: function (field) {
-						const fileSelected = field.files[0];
-						if (field) {
+						const fileSelected = field.files && field.files[0];
+						if (fileSelected) {
 							return {invalid: false, message: ''}
 						} else {
 							return {invalid: true, message: 'Avatar is required'}
 						}
 					},
-					// fileSize: function (field) {
-					// 	const fileSelected = field.files[0];
-					// 	const validFileSize = fileSelected.size <= 500000;
-					// 	if (validFileSize.test(field)) {
-					// 		return {invalid: false, message: ''}
-					// 	} else {
-					// 		return {invalid: true, message: 'File is too large. Please upload a photo under 500kb'}
-					// 	}
-					// },
-					// fileType: function (field) {
-					// 	const fileSelected = field.files[0];
-					// 	const validExtensions = /(\.JPG|\.PNG|\.jpeg)$/g
-					// 	if (validExtensions.test(field)) {
-					// 		return {invalid: false, message: ''}
-					// 	} else {
-					// 		return {invalid: true, message: 'Wrong file type. Please upload a JPG or PNG file'}
-					// 	}
-					// },
+					fileSize: function (field) {
+						const fileSelected = field.files && field.files[0];
+						if (!fileSelected) return {invalid: false, message: ''};
+						const validFileSize = fileSelected.size <= 500000;
+						if (validFileSize) {
+							return {invalid: false, message: ''}
+						} else {
+							return {invalid: true, message: 'File is too large. Please upload a photo under 500kb'}
+						}
+					},
+					fileType: function (field) {
+						const fileSelected = field.files && field.files[0];
+						if (!fileSelected) return {invalid: false, message: ''};
+						const validExtensions = /(\.jpg|\.JPG|\.PNG|\.jpeg|\.png)$/i
+						if (validExtensions.test(fileSelected.name)) {
+							return {invalid: false, message: ''}
+						} else {
+							return {invalid: true, message: 'Wrong file type. Please upload a JPG or PNG file'}
+						}
+					},
 				}
 			},
 			fullName: {
@@ -90,7 +98,16 @@ window.getTicket = function() {
 		},
 		validate(field) {
 			for (const key in this.validation[field].rule) {
-				const validationResult = this.validation[field].rule[key](this[field])
+				let fieldValue = this[field];
+				if (field === 'avatar') {
+					fieldValue = document.getElementById('upload_avatar');
+					let reader = new FileReader();
+					reader.onload = function(e) {
+						this.avatar = e.target.result;
+					}.bind(this);
+					this.avatarPreview = reader.readAsDataURL(fieldValue.files[0]);
+				}
+				const validationResult = this.validation[field].rule[key](fieldValue)
 				if (validationResult.invalid) {
 					this.validation[field].invalid = true
 					this.validation[field].message = validationResult.message
@@ -100,19 +117,51 @@ window.getTicket = function() {
 				this.validation[field].message = ''
 			}
 		},
+		handleFileSelect(event) {
+			const file = event.target.files[0];
+
+			if (file) {
+				this.validate('avatar');
+				if (!this.validation.avatar.invalid) {
+					const reader = new FileReader();
+					reader.onload = (e) => {
+						this.avatarPreview = e.target.result;
+					};
+					reader.onerror = (e) => {
+						this.avatarPreview = null;
+					};
+					reader.readAsDataURL(file);
+					this.avatar = file.name;
+				} else {
+					this.avatarPreview = null;
+					this.avatar = '';
+				}
+			} else {
+				this.avatarPreview = null;
+				this.avatar = '';
+			}
+		},
+		removeAvatar() {
+			this.avatarPreview = null;
+			this.avatar = '';
+			document.getElementById('upload_avatar').value = '';
+			this.validation.avatar.invalid = false;
+			this.validation.avatar.message = '';
+		},
 		submit() {
-			// this.validate('avatar')
+			this.validate('avatar')
 			this.validate('fullName')
 			this.validate('email')
 			this.validate('GHUsername')
 			if (
-				// this.validation['avatar'].invalid === false &&
+				this.validation['avatar'].invalid === false &&
 				this.validation['fullName'].invalid === false &&
 				this.validation['email'].invalid === false &&
 				this.validation['GHUsername'].invalid === false
 			) {
 				this.requestEarlyAccess = {
 					avatar: this.avatar,
+					avatarPreview: this.avatarPreview,
 					fullName: this.fullName,
 					email: this.email,
 					GHUsername: this.GHUsername
